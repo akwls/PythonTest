b8bc65149ea8a2be6413d51e33c1f1673165754b
akwls <rhrnak624@naver.com>
2021-06-02 20:31:22 +0900

    0602

---
+++ Part2. 변수와 계산/number.py
@@ -0,0 +1,19 @@
+a=33
+b=3
+
+summation = a+b
+ 
+multiply = a*b
+
+divide = a/b
+
+remainder = a%b
+
+power = a**b
+
+
+print("summation은 {}입니다.".format(summation))
+print("multiply는 {}입니다.".format(multiply))
+print("divide는 {}입니다.".format(divide))
+print("remainder는 {}입니다.".format(remainder))
+print("power는 {}입니다.".format(power))
