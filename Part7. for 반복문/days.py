88d15eabfd2a5035239b625f791abe50616e059a
akwls <rhrnak624@naver.com>
2021-06-09 22:43:07 +0900

    0609

---
+++ Part7. for 반복문/days.py
@@ -0,0 +1,5 @@
+days = [31,29,31,30,31,30,31,31,30,31,30,31]
+
+
+for i in range(len(days)):
+    print('{}월의 날짜수는 {}일 입니다.'.format( i+1 , days[i] ))
---
+++ Part7. for 반복문/for~in.py
@@ -0,0 +1,3 @@
+list = ['가위','바위','보']
+for n in list:
+    print(n)
